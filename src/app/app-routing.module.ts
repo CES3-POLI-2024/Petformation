39b048c6865ba8fa98aa7d0f5f3008f5cee2a937
danielmarin06 <daniel_marin23162@elpoli.edu.co>
2024-03-07 20:28:08 -0500

    Primera carga a github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesListComponent } from './components/clientes/clientes-list/clientes-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path:'',
+  //   component: ClientesListComponent
+  // },
+  {
+    path:'clientes',
+    component: ClientesListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/models/clientes.model.ts
@@ -0,0 +1,8 @@
+export interface clientes{
+    ID_CLIENTE: number;
+    CEDULA_CLIENTE: string;
+    NOMBRE_CLIENTE: string;
+    TELEFONO_CLIENTE: string;
+    DIRECCION_CLIENTE: string;
+    CORREO_CLIENTE: string;
+}
